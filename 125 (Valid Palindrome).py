1cbc48ae8228eed4aae5ee1f65de81bd957c99bf
EndlayNavam <143595373+EndlayNavam@users.noreply.github.com>
2026-05-30 23:37:22 +0530

    Add files via upload

---
+++ 125 (Valid Palindrome).py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        n = len(s)
+        i, j = 0, n - 1
+
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            else:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+        
+        return True
